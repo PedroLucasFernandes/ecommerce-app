680a03289bd45a660c026f06a2723fc50e74b214
Pedro Lucas Fernandes <plff201100@gmail.com>
2024-03-13 13:37:57 -0300

    feat: block routes for not login users

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,21 @@ const productRoutes = require('./productRoutes');
 const orderRoutes = require('./orderRoutes');
 const customerRoutes = require('./customerRoutes');
 const loginRoutes = require('./loginRoutes');
+const { loginUsers } = require('../controller/loginController');
 
-router.use('/product', productRoutes);
-router.use('/order', orderRoutes);
-router.use('/customer', customerRoutes);
+const authenticateAPIMiddleware = (req, res, next) => {
+    const sessionToken = req.cookies.session_id;
+    const isAuthenticated = loginUsers.some(user => user.sessionToken === sessionToken);
+    if (isAuthenticated) {
+        next();
+    } else {
+        res.status(401).json({ error: 'Acesso não autorizado' });
+    }
+};
+
+router.use('/product', authenticateAPIMiddleware, productRoutes);
+router.use('/order', authenticateAPIMiddleware, orderRoutes);
+router.use('/customer', authenticateAPIMiddleware, customerRoutes);
 router.use('/login', loginRoutes);
 
 module.exports = router;
